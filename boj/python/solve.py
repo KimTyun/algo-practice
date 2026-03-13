f751682f56d4fac52ccab2738a1a7bd1b788a2d5
KimTyun <106860407+KimTyun@users.noreply.github.com>
2026-03-13 18:07:46 +0900

    0313

--- boj/python/solve.py
+++ boj/python/solve.py
@@ -1,4 +1,4 @@
-#24416 알고리즘 수업 -피보나치 수1
+#1912 연속합
 import sys
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -6,14 +6,20 @@ sys.stdin = open(os.path.join(BASE_DIR, "input.txt"), "r")
 
 N= int(sys.stdin.readline())
 
-def fibonacci(n):
-    f = list(range(n+1))
-    f[1] = 1
-    f[2] = 1
-    for i in range(3,n+1):
-        f[i] = f[i-1]+f[i-2]
-    return f[n]
+numbers = [int(x) for x in sys.stdin.readline().split()]
+# dp[n]은 numbers[n]까지의 누적합 중 최댓값
+# numbers[10 -4 3 1 5 6 -35 12 21 -1]면
+# dp [10,6,9,10,15,21,-14,12,33,32]
 
+# dp[0]은 numbers[0]이고, dp[i]는 dp[i-1]+numbers[i]와 numbers[i]중 큰 수임.
+# answer는 dp의 최댓값.
 
+answer = numbers[0]
+dp = [0 for x in range(N)]
+dp[0] = numbers[0]
+for i in range(1,N):
+    dp[i] = dp[i-1]+numbers[i] if dp[i-1]+numbers[i]> numbers[i] else numbers[i]
+    if dp[i]>answer: answer=dp[i]
 
-print(fibonacci(N), N-2 if N>=3 else 1)
+print(max(dp))
+
